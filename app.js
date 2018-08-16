b2bb5b79e230c74915f89a5afb22c04b2cfc00b3
Anthony Lapham <lapham.2@wright.edu>
2018-08-16 17:04:23 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+
+io.sockets.on('connection', (socket) => {
+  let number = 0;
+
+  socket.emit('welcomeUser', { msg: 'Thanks for connecting' });
+
+  socket.on('increaseNumberToServer', function() {
+    number++;
+    io.emit('increaseNumberToClient', { counter: number });
+
+    socket.on('decreaseNumberToServer', function(){
+      number--;
+      io.emit('decreaseNumberToServer', {counter: number});
+    });
+  });
+})
+
+app.get('*', (req, res) => {
+  res.render('index');
+});
+
+server.listen(3000, () => {
+  console.log('Express server is running!');
+});
---
+++ public/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  const socket = io();
+
+  socket.on('welcomeUser', function(data) {
+    console.log('This is data from the server:', data.msg)
+  });
+
+  socket.on('increaseNumberToClient', function(data) {
+    console.log('does this work?', data);
+    $('#counter').text(data.counter);
+  });
+
+  socket.on('decreaseNumberToClient', function(newData){
+    $('#counter').text(newData.counter);
+  })
+
+  $('#incrementBtn').on('click', function() {
+    socket.emit('increaseNumberToServer')
+  });
+
+  $('.decrementBtn').on('click', function(){
+    socket.emit('decreaseNumberToServer')
+  });
+});
